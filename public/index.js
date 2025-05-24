09549d9af1c47adb90d199228a874d027fb647aa
tonakihan <116086641+tonakihan@users.noreply.github.com>
2025-05-24 09:45:34 +0900

    Add JS

--- public/index.js
+++ public/index.js
@@ -1,1 +1,39 @@
 "use strict";
+
+async function getData() {
+  const res = await fetch("public/data.json");
+  return res.json();
+}
+
+function insertScoreInDocument(nameField, value) {
+  const line = document.getElementById(nameField.toLowerCase());
+  const el = line.getElementsByClassName("score")[0].children[0];
+  el.innerHTML = value;
+}
+
+function updateTotalScore(jsonData) {
+  let score = jsonData.reduce(
+    (acc, crrEl) => (acc + crrEl.score) / 2,
+    jsonData[0].score,
+  );
+  score = Math.floor(score);
+
+  insertScoreInDocument("total-score", score);
+}
+
+getData().then((data) => {
+  function work() {
+    updateTotalScore(data);
+    data.map((el) => {
+      insertScoreInDocument(el.category, el.score);
+    });
+  }
+
+  if (document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", (_) => {
+      work();
+    });
+  } else {
+    work();
+  }
+});
